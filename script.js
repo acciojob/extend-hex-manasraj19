724493a9d99a9375a7143208a0c38bbca3ee1bc8
Manas Raj <119037651+manasraj19@users.noreply.github.com>
2024-01-06 12:15:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const extendHex = (shortHex) => {
   // write your code here
+  shortHex = shortHex.replace(/^#/, '');
+
+  if(!/^[0-9a-fA-F]{3}$/.test(shortHex)){
+    return "Invalid Input";
+  }
+
+  const fullhex= shortHex.split('').map(char => char.repeat(2)).join('');
+
+  return `#${fullhex}`;
 };
 
 // Do not change the code below.
